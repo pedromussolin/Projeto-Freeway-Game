8d083b550e61fa0ea7666cfc8e5e040d056e7ebd
Pedro Henrique Mussolin Ayres Netto <104608112+pedromussolin@users.noreply.github.com>
2022-10-04 23:47:16 -0300

    Create imagens.js

---
+++ imagens.js
@@ -0,0 +1,25 @@
+//código das imagens e sons do jogo
+
+let imageDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+//sons
+let somDaTrilha;
+let somDaColisao;
+let somDoPonto
+
+function preload(){
+  imagemDaEstrada =          loadImage("images/estrada.png");
+  imagemDoAtor = loadImage("images/ator-1.png");
+  imagemCarro = loadImage("images/carro-1.png");
+  imagemCarro2 = loadImage("images/carro-2.png");
+  imagemCarro3 = loadImage("images/carro-3.png");
+  imagemCarros = [imagemCarro, imagemCarro2,   imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3];
+  
+  somDaTrilha = loadSound("sons/trilha.mp3");
+  somDaColisao = loadSound("sons/colidiu.mp3");
+  somDoPonto = loadSound("sons/pontos.wav");
+}
